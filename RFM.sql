3c26323d20b56525333798593804f87e166f0b53
RomaniiEkaterina <katushar2801@icloud.com>
2025-09-27 20:33:05 +0700

    Add files via upload

---
+++ RFM.sql
@@ -0,0 +1,54 @@
+            
+             with Recency_Frequency_Monetary as( 
+     select distinct(card) as card,
+     '2022-06-10'- max(datetime::date) as Recency,
+     count(datetime) as frequency,
+     sum(summ_with_disc) as monetary
+     from bonuscheques
+     group by card
+     )
+                          select Recency, frequency, monetary, count (card)
+                     from Recency_Frequency_Monetary
+                     group by Recency, frequency, monetary
+                     order by monetary desc, Recency desc, frequency desc
+                                                          
+                                             
+    with data as( 
+     select distinct(card) as card,
+     '2022-06-10' - max(datetime::date) as recency,
+     count(datetime) as frequency,
+     sum(sum(summ_with_disc)) over (order by sum(summ_with_disc) desc)/sum(sum(summ_with_disc))over() as monetary
+     from bonuscheques
+     group by card
+     having char_length(card) < 14
+             ),
+                          RFM as (select 
+                          case when  recency < 15 then '3' 
+                          when  recency < 61 then '2' 
+                          when  recency >= 61  then '1' end as recency,
+                          case when frequency > 12 then '3' 
+                          when frequency > 6 then '2' 
+                          when frequency <7 then '1' end as frequency,
+                          case when monetary <= 0.65 then '3' 
+                          when monetary <= 0.9 then '2' 
+                          when monetary >0.9 then '1' end as monetary
+                          from data
+                      )
+                                             select  recency,  frequency,  monetary,  count (*)
+                                             from rfm 
+                                             group by recency,  frequency,   monetary
+                                             order by monetary desc, Recency desc, frequency desc
+                                                                            
+                                             
+                                             
+                                             
+
+     
+     
+     
+                                        
+                                 
+                             
+            
+     
+   
